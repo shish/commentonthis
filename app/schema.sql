16edc4286dc2d327698a89481e95325a26931236
Shish <shish@shishnet.org>
2012-05-23 23:06:28 +0100

    message table, and enforce user IDs

--- app/schema.sql
+++ app/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS "comment";
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS "session";
+DROP TABLE IF EXISTS "message";
 
 CREATE TABLE "user" (
 	id SERIAL PRIMARY KEY,
@@ -13,7 +14,7 @@ CREATE TABLE "comment" (
 	page_owner_id INTEGER NOT NULL REFERENCES "user"(id),
 	page_url TEXT NOT NULL,
 	item_id TEXT NOT NULL,
-	commenter_id INTEGER REFERENCES "user"(id),
+	commenter_id INTEGER NOT NULL REFERENCES "user"(id),
 	content TEXT NOT NULL,
 	date_posted TIMESTAMP NOT NULL DEFAULT now(),
 	private BOOLEAN NOT NULL DEFAULT FALSE
@@ -22,4 +23,11 @@ CREATE TABLE "session" (
 	session_id CHAR(128) UNIQUE NOT NULL,
 	atime TIMESTAMP NOT NULL default current_timestamp,
 	data TEXT
+);
+CREATE TABLE "message" (
+	id SERIAL PRIMARY KEY,
+	user_id_from INTEGER NOT NULL REFERENCES "user"(id),
+	user_id_to INTEGER NOT NULL REFERENCES "user"(id),
+	date_posted TIMESTAMP NOT NULL DEFAULT now(),
+	content TEXT NOT NULL
 );
